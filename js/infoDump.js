d6e39ba47ccf45439d63b813750ef2451e68dd6d
anchovii <108648969+anchovii@users.noreply.github.com>
2023-12-23 01:36:14 -0700

    wawascript

---
+++ js/infoDump.js
@@ -0,0 +1,32 @@
+//a silly for every character
+//every character i have more than like 2 sentences to talk about, in any case
+
+
+
+function acaudInfo(){
+    var theSwitch = document.getElementById('acaudSwitch');
+    var theLight = document.getElementById('acaudLight');
+
+    if (theLight.style.display === "none"){
+        theLight.style.display = 'inline';
+        theSwitch.innerHTML = "too much red.";
+    }else{
+        theLight.style.display = 'none';
+        theSwitch.innerHTML = "can you say that again?";
+    }
+}
+
+
+
+function mechaeInfo(){
+    var theSwitch = document.getElementById('mechaeSwitch');
+    var theLight = document.getElementById('mechaeLight');
+
+    if (theLight.style.display === "none"){
+        theLight.style.display = 'inline';
+        theSwitch.innerHTML = "seen enough?";
+    }else{
+        theLight.style.display = 'none';
+        theSwitch.innerHTML = "read again?";
+    }
+}
